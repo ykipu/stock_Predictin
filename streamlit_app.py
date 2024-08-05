730aa1493f41c4c76df0c5b69e7c24c6d8095f38
ykipu <166149735+ykipu@users.noreply.github.com>
2024-08-05 22:32:35 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,76 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import numpy as np
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import yfinance as yf
+from datetime import datetime
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+st.title("Stock Price Predictor App")
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+stock = st.text_input("Enter the stock ID", 'NVDA')
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+end = datetime.now()
+start = datetime(end.year-20, end.month, end.day)
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+nvidia_data = yf.download(stock, start, end)
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+model = load_model("Latest_stock_price_model.keras")
+st.subheader("Stock Data")
+st.write(nvidia_data)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+splitting_len = int(len(nvidia_data)*0.7)
+x_test = pd.DataFrame(nvidia_data.Close[splitting_len:])
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+def plot_graph(figsize, values, full_data, extra_data=0, extra_dataset=None):
+    fig = plt.figure(figsize=figsize)
+    plt.plot(values, 'Orange')
+    plt.plot(full_data.Close, 'b')
+    if extra_data:
+        plt.plot(extra_dataset)
+    return fig
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+st.subheader('Original Close Price and MA for 250 days')
+nvidia_data['MA_for_250_days'] = nvidia_data.Close.rolling(250).mean()
+st.pyplot(plot_graph((15,6), nvidia_data['MA_for_250_days'], nvidia_data, 0))
 
-    return gdp_df
+st.subheader('Original Close Price and MA for 200 days')
+nvidia_data['MA_for_200_days'] = nvidia_data.Close.rolling(200).mean()
+st.pyplot(plot_graph((15,6), nvidia_data['MA_for_200_days'], nvidia_data, 0))
 
-gdp_df = get_gdp_data()
+st.subheader('Original Close Price and MA for 100 days')
+nvidia_data['MA_for_100_days'] = nvidia_data.Close.rolling(100).mean()
+st.pyplot(plot_graph((15,6), nvidia_data['MA_for_100_days'], nvidia_data, 0))
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+st.subheader('Original Close Price and MA for 250 days and MA for 250 days')
+st.pyplot(plot_graph((15,6), nvidia_data['MA_for_100_days'], nvidia_data, 1, nvidia_data['MA_for_250_days']))
 
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(x_test)
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
+x_data = []
+y_data = []
 
-# Add some spacing
-''
-''
+for i in range(100, len(scaled_data)):
+    x_data.append(scaled_data[i-100:i])
+    y_data.append(scaled_data[i])
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+x_data, y_data = np.array(x_data), np.array(y_data)
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+predictions = model.predict(x_data)
 
-countries = gdp_df['Country Code'].unique()
+inv_pre = scaler.inverse_transform(predictions)
+inv_y_test = scaler.inverse_transform(y_data)
 
-if not len(countries):
-    st.warning("Select at least one country")
+ploting_data = pd.DataFrame({
+    'original_test_data': inv_y_test.reshape(-1),
+    'predictions': inv_pre.reshape(-1)
+}, index = nvidia_data.index[splitting_len+100:])
+st.subheader("Original values vs Predicted values")
+st.write(ploting_data)
 
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+st.subheader('Original Close price vs Predicted Close price')
+fig = plt.figure(figsize=(15,6))
+plt.plot(pd.concat([nvidia_data.Close[:splitting_len+100], ploting_data['original_test_data']], axis=0), label='Original')
+plt.plot
